e279c85e729e5af39a78168aec8b65368ca9f632
AnubhavJoria <anubhav.joria@innobitsystems.com>
2023-07-04 18:06:21 +0530

    client-code implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
+import React, { useEffect } from "react";
 
-import {getProviders} from 'next-auth/react'
-
+import { getProviders } from "next-auth/react";
+import Feed from "@/components/Feed";
 
 function Home() {
-
-  const prov = async() =>{
-    const providers = await getProviders();
-    console.log(providers); 
-  }
-  useEffect(()=>{
-   prov();
-  },[])
-
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
 }
 
-export default Home
+export default Home;
