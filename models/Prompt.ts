b5fe777f6165c75ec6a33e409e4bb903a9509387
Anubhav-Joria <91547784+Anubhav-Joria@users.noreply.github.com>
2023-07-05 00:12:58 +0530

    create prompt completed

---
+++ models/Prompt.ts
@@ -0,0 +1,19 @@
+import { Schema, model, models } from "mongoose";
+
+const promptSchema = new Schema({
+  prompt: {
+    type: String,
+    required: [true, "Required field"],
+  },
+  userId: {
+    type: String,
+    required: [true, "Id is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is a Required field"],
+  },
+});
+
+const Prompts = models.Prompts || model('Prompts', promptSchema)
+export default Prompts;
